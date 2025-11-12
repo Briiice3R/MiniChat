9110f7b792ab63694297b6d4d2833269f0e5d6c6
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-11-12 08:30:30 +0100

    feat: Adding .env file

--- bddConnection.php
+++ bddConnection.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $bdd = new PDO("mysql:host=localhost;dbname=minichat", "root", "");
+    $bdd = new PDO("mysql:host=".apache_getenv("HOST").";dbname=".apache_getenv("DBNAME").",".apache_getenv("LOGIN"), apache_getenv("PASSWORD"));
 } catch(\Exception $e){
     die("Message d'erreur : " . $e->getMessage());
 }
